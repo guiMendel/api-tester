89466454f270142738e01b01e08c9a55cf0ca884
guimendel <guimendeln@gmail.com>
2021-03-09 12:28:08 +0100

    feat/ erros indicados nos campos de criar usuario

--- src/views/NewUser.vue
+++ src/views/NewUser.vue
@@ -17,21 +17,37 @@
 
       <label for="name">Name</label>
       <input type="text" id="name" v-model="name" />
+      <!-- as mensagens de erro aparecem se o error -->
+      <small v-show="error_message(name, 'name')">{{
+        error_message(name, "name")
+      }}</small>
 
       <label for="email">Email</label>
       <input type="email" id="email" v-model="email" />
+      <small v-show="error_message(email, 'email')">{{
+        error_message(email, "email")
+      }}</small>
 
       <label for="nickname">Nickname</label>
       <input type="text" id="nickname" v-model="nickname" />
+      <small v-show="error_message(nickname, 'nickname')">{{
+        error_message(nickname, "nickname")
+      }}</small>
       <em class="tip"
         >Your nickname is how other users will be able to look you up!</em
       >
 
       <label for="age">Age</label>
       <input type="number" id="age" v-model="age" />
+      <small v-show="error_message(age, 'age')">{{
+        error_message(age, "age")
+      }}</small>
 
       <label for="password">Password</label>
       <input type="password" id="password" v-model="password" />
+      <small v-show="error_message(password, 'password')">{{
+        error_message(password, "password")
+      }}</small>
     </div>
   </curtain-view>
 </template>
@@ -53,6 +69,27 @@ export default {
     email: "",
   }),
   methods: {
+    error_message(value, field) {
+      if (!value) return "";
+
+      const field_handlers = {
+        nickname() {
+          // IMPLEMENTAR checagem se eh unico
+          return "";
+        },
+        age: () => (value >= 18 ? "" : "User must be 18 or older"),
+        password: () =>
+          value.length >= 6
+            ? ""
+            : "Password must be at least 6 characters long",
+        // IMPLEMENTAR checagem se eh unico no email
+        email: () => (value.match(/.@./) ? "" : "Invalid email address"),
+      };
+
+      const handler = field_handlers[field] ?? (() => "");
+
+      return handler();
+    },
     // faz a solicitacao de criar usuario para a API
     submitUser() {
       axios
@@ -64,12 +101,16 @@ export default {
           email: this.email,
         })
         .then((response) => {
-          console.log(response)
-          this.$toast.success(`Successfully created user ${response.data.user.name}`)
+          console.log(response);
+          this.$toast.success(
+            `Successfully created user ${response.data.user.name}`
+          );
         })
         .catch((error) => {
-          console.log({error})
-          this.$toast.error(`Check the network panel for details. Error: ${error.message}`)
+          console.log({ error });
+          this.$toast.error(
+            `Check the network panel for details. Error: ${error.message}`
+          );
         });
     },
   },
@@ -103,5 +144,11 @@ input {
 
   width: 100%;
   padding: 0.5rem 0.5rem;
+}
+
+small {
+  font-size: 1rem;
+  font-style: italic;
+  color: var(--text-red)
 }
 </style>
